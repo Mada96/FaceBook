8fc3b9d7a3875d5de6b5d90772b534ea672d40ca
Mohamed Nasser <madanasser81@gmail.com>
2018-04-10 20:01:23 +0200

    intial commit

---
+++ FaceBooWEB_3raye2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FaceBooWEB_3raye2.MvcApplication" Language="C#" %>
---
+++ FaceBooWEB_3raye2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FaceBooWEB_3raye2.Startup))]
+namespace FaceBooWEB_3raye2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
